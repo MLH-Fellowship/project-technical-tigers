058486307b1d3ce3b5ee886e78eda998df811c34
BrandonMagana <magana.brandon12@outlook.com>
2022-06-19 23:02:22 -0500

    redeploy-site.sh using detattached session

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-tmux kill-session -t flask; 
+#kill tmux sessions
+pkill -9 tmux
+#update repo
 cd project-technical-tigers
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new -s flask 'flask run --host=0.0.0.0'
+#new tmux session
+tmux new -d -s flask 'flask run --host=0.0.0.0'
