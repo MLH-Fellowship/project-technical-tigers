528508a1afd17b5ef2d6221e0c84783b39f18fb3
BrandonMagana <magana.brandon12@outlook.com>
2022-06-24 18:06:45 -0500

    Add Timeline posts CRUD endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,35 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 from dotenv import load_dotenv
-import random
 from app.Data_Loader import Data_Loader
+import datetime
+from peewee import *
+from playhouse.shortcuts import model_to_dict
+
 load_dotenv()
 app = Flask(__name__)
 
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"), 
+    user = os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host = os.getenv("MYSQL_HOST"),
+    port = 3306
+)
 
-DATA = Data_Loader()    
+class TimeLinePost(Model):
+    name = CharField() 
+    email = CharField()
+    content = CharField()
+    created_at = DateTimeField(default=datetime.datetime.now)
 
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimeLinePost])
+
+DATA = Data_Loader()
 
 @app.route('/')
 def landing_page():
@@ -25,3 +46,34 @@ def projects():
 @app.route('/projects')
 def education():
     return render_template('projects.html', data = DATA.projects_data, url=os.getenv('URL'))
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']   
+    timeline_post = TimeLinePost.create(name=name, email=email, content=content)
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_timeline_post():
+    return {
+        'timeline_posts':[
+            model_to_dict(post) 
+            for post in 
+            TimeLinePost.select().order_by(TimeLinePost.created_at.desc())
+        ],
+    }
+
+@app.route('/api/timeline_post', methods=['DELETE'])
+def delete_timeline_post():
+    name = request.form['name']
+    try:
+        post = TimeLinePost.get(TimeLinePost.name == name)
+        post.delete_instance()
+    except:
+        return Response(
+            "Post not found",
+            status=400,
+        )
+    return {"mesagge" : "deleted", "post":model_to_dict(post)}
