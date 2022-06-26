de5b2be7b7820ab37efea0117a777502a574fd14
BrandonMagana <magana.brandon12@outlook.com>
2022-06-25 21:21:41 -0500

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#kill tmux sessions
-pkill -9 tmux
+
 #update repo
 cd project-technical-tigers
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-#new tmux session
-tmux new -d -s flask 'flask run --host=0.0.0.0'
+
+#restarting service
+systemctl restart myportfolio
