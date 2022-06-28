a2a8b456d8203b9217afb5525c03ea350a7992e1
BrandonMagana <magana.brandon12@outlook.com>
2022-06-27 21:07:37 -0500

    Landing page v2

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,7 +1,8 @@
-const navMenu = document.getElementById('nav-menu')
-const navToggle = document.getElementById('nav-toggle')
-const navClose = document.getElementById('nav-close')
-const navLink = document.querySelectorAll('.nav-link')
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+const navLink = document.querySelectorAll('.nav-link');
+const scrollUp= document.getElementById('scroll-up');
 
 if(navToggle){
     navToggle.addEventListener('click', () =>{
@@ -18,3 +19,13 @@ if(navClose){
 navLink.forEach(n => n.addEventListener('click', ()=>{
     navMenu.classList.remove('show-menu')
 }))
+
+const scrollUpEvent = () =>{
+    if(this.scrollY >= 50){
+        scrollUp.classList.add('show-scroll')
+    }else{
+        scrollUp.classList.remove('show-scroll');
+    } 
+}
+
+window.addEventListener('scroll', scrollUpEvent)
