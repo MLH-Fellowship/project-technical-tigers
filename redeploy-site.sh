19a8a3a938782da8a1b96bdbfdbc23fa279eaaa7
BrandonMagana <magana.brandon12@outlook.com>
2022-07-05 19:24:42 +0000

    redeploy script using docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,8 +3,5 @@
 #update repo
 cd project-technical-tigers
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-
-#restarting service
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
