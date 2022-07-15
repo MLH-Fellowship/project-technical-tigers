f224d9e91df77c3bfa890de80fc085c8b5a80f95
BrandonMagana <magana.brandon12@outlook.com>
2022-07-14 20:04:10 -0500

    Clean up working directory

--- app/__init__.py
+++ app/__init__.py
@@ -48,26 +48,9 @@ def landing_page():
     DATA = Data_Loader()
     return render_template('home.html',  data= DATA.skills_data)
     
-
-@app.route('/about-me')
-def about_me():
-    return render_template('_about-me.html', data= DATA.about_me_data, url=os.getenv('URL'))
-
-@app.route('/education-and-work-expirience')
-def projects():
-    return render_template('_education-and-work-expirience.html', data = DATA.expiriences_data, url=os.getenv('URL'))
-
-@app.route('/projects')
-def education():
-    return render_template('_projects.html', data = DATA.projects_data, url=os.getenv('URL'))
-
 @app.route('/timeline')
 def timeline():
     return render_template('_timeline.html', data= DATA.about_me_data, title='Timeline', url=os.getenv('URL'))
-
-@app.route('/base')
-def base():
-    return render_template('_landingPage.html', data = DATA.landing_page_data, landing_page=True, url=os.getenv('URL'))
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
